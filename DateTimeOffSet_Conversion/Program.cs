7ae2614361ec2274e5cfb29faa1bd16433b084e5
Duane Allman Billue <dbillue@gmail.com>
2022-01-04 04:32:17 -0500

    Initial check in

---
+++ DateTimeOffSet_Conversion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DateTimeOffSet_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTimeOffset startDate = DateTime.Now;
+            DateTimeOffset endDate = DateTime.Now;
+
+            DateTime dtStartDate = DateTime.Now;
+            DateTime dtEndDate = DateTime.Now;
+
+            startDate = DateTime.Parse(startDate.ToString("yyyy-MM-dd"));
+            dtStartDate = DateTime.Parse(dtStartDate.ToString("yyyy-MM-dd"));
+
+            Console.WriteLine("{0}" + Environment.NewLine + "{1}", startDate.ToString(), endDate.ToString());
+            Console.WriteLine("{0}", startDate.ToString("yyyy-MM-dd"));
+            Console.WriteLine("{0}" + Environment.NewLine + "{1}", dtStartDate.ToString(), dtEndDate.ToString());
+            Console.WriteLine("{0}", dtStartDate.ToString("yyyy-MM-dd"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# DateTimeOffSet_Conversion
+ 
